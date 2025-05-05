f1280ba6a6be6b5ec67712aa8e6b197d18f9909b
German-Lecherbauer <gerlecherbauer@gmail.com>
2025-05-04 21:35:20 -0300

    Media Query

---
+++ styles/scripts/app.js
@@ -0,0 +1,25 @@
+
+const products = [
+    {
+        nombre: "Remera de Compresion",
+        imagen: "https://github.com/German-Lecherbauer/Web-Deportes/blob/216adf74abb03b72f01f902cd72a544b849e07d8/img/remera-compresion.png",
+        precio: 40000
+    },
+
+    {
+        nombre: "Zapatillas AirFly",
+        imagen: "https://github.com/German-Lecherbauer/Web-Deportes/blob/216adf74abb03b72f01f902cd72a544b849e07d8/img/zapatilla-airfly.png",
+        precio: 120000
+    },
+    {
+        nombre: "Reloj Garmin",
+        imagen: "https://github.com/German-Lecherbauer/Web-Deportes/blob/216adf74abb03b72f01f902cd72a544b849e07d8/img/reloj-garmin.png",
+        precio: 300000
+    }, 
+    {
+        nombre: "Short Deportivo Negro",
+        imagen: "https://github.com/German-Lecherbauer/Web-Deportes/blob/216adf74abb03b72f01f902cd72a544b849e07d8/img/short-deportivo.png",
+        precio: 300000
+    }
+
+]
